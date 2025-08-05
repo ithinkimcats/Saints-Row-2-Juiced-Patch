a8adf1c24563086ff6cfa6a9cfdb72619a1aad21
Clippy95 <qpzezoqp15@gmail.com>
2025-08-06 00:26:32 +0300

    7.9.3

--- Monkey Patch/UtilsGlobal.h
+++ Monkey Patch/UtilsGlobal.h
@@ -56,7 +56,7 @@ inline bool isPaused;
 #if JLITE
 inline const char* juicedversion = "1.5.1";
 #else
-inline const char* juicedversion = "7.9.0";
+inline const char* juicedversion = "7.9.3";
 #endif
 
 inline const char* thaRowmenuversion = "0.0.1";
